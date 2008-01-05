613cbb1235a0f0af2329855e650cd7e4946f389c
fred.morcos <fred.morcos@1f48e373-bc42-0410-b37a-178097bd8f8e>
2008-01-05 12:54:36 +0000

    pacman in a struct now

--- src/man.h
+++ src/man.h
@@ -1,18 +1,39 @@
+/*
+ * This file is part of pacman-ng
+ * 
+ * pacman-ng A 3D version of the pacman game
+ * Copyright (C) 2008	Frederic-Gerald Morcos (fredmorcos)
+ * 						Marleine Mounir Daoud
+ * 						Andrew Botros Boktor
+ * 
+ * pacman-ng is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * pacman-ng is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with pacman-ng.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __MAN
 #define __MAN
 
-/* 
- * the pacman is either a sphere or a fat circle,
- * it has a color, a radius, a place and a rotation
- */
+#include "global.h"
+
 struct _man
 {
-	int x, y, z;
-	int radius;
-	float x_r, x_y, z_r;
-	float r, g, b;
+	float x, y, z;
+	float radius;
+	boolean evil;
 };
 
 typedef struct _man man;
+
+void man_draw (man *m);
 
 #endif
