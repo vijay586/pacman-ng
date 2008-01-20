2a4e0aea32dab8637a4dfa9ea6e0cecf75a59190
fred.morcos <fred.morcos@1f48e373-bc42-0410-b37a-178097bd8f8e>
2008-01-20 09:49:03 +0000

    why do we still use glut when we have sdl???

---
+++ branches/pacman-ng-sdl/src/wall.h
@@ -0,0 +1,51 @@
+/*
+ * This file is part of pacman-ng
+ * 
+ * pacman-ng A 3D version of the pacman game
+ * Copyright (C) 2008	Frederic-Gerald Morcos (fredmorcos)
+ * 						Marleine Mounir Daoud
+ * 						Andrew Botros Boktor
+ * 
+ * pacman-ng is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * pacman-ng is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with pacman-ng.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __WALL
+#define __WALL
+
+#include "global.h"
+
+enum _orientation
+{
+	VERTICAL = 0,
+	HORIZONTAL = 1
+};
+typedef enum _orientation orientation;
+
+struct _wall
+{
+	point pos;
+	orientation orient;
+	float length;
+};
+typedef struct _wall wall;
+
+
+
+
+#define WALL_THICKNESS 0.3
+#define WALL_HEIGHT 1
+
+void wall_draw ( wall *w );
+
+#endif
