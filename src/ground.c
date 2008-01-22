ad4bbb1fc9db1fc50c42b1c459f627bb75702d6d
fred.morcos <fred.morcos@1f48e373-bc42-0410-b37a-178097bd8f8e>
2008-01-22 14:20:55 +0000

    old stuff

--- src/ground.c
+++ src/ground.c
@@ -1,6 +1,7 @@
 #include "ground.h"
 
 #include <GL/freeglut.h>
+#include <stdlib.h>
 
 void renderGround (ground *g)
 {
@@ -10,4 +11,13 @@ void renderGround (ground *g)
 	glScalef (g->fWidth, g->fHeight, g->fDepth);
 	glutSolidCube (1);
 	glPopMatrix ();
+}
+
+ground *newGround (float fW, fH, fD)
+{
+	ground *temp = calloc (1, sizeof (ground));
+	temp->fWidth = fW;
+	temp->fHeight = fH;
+	temp->fDepth = fD;
+	return temp;
 }
