76a6b5a5279f2135654652ee3b2f47d5f9b4154a
marleine <marleine@1f48e373-bc42-0410-b37a-178097bd8f8e>
2008-01-22 20:25:21 +0000

    top view not working yet

--- src/glut-extra.c
+++ src/glut-extra.c
@@ -8,5 +8,7 @@ void initGlutWindow (int iWidth, int iHeight)
 	glutInitWindowSize (iWidth, iHeight);
 	glutInitWindowPosition ((glutGet (GLUT_SCREEN_WIDTH) - iWidth) / 2, 
 							(glutGet (GLUT_SCREEN_HEIGHT) - iHeight) / 2);
-	glutCreateWindow ("Pacman NG");
+	mainWindow = glutCreateWindow ("Pacman NG");
+	
+	subWindow = glutCreateSubWindow (mainWindow, 0, 0, 200, 150);
 }
--- src/glut-extra.h
+++ src/glut-extra.h
@@ -1,6 +1,9 @@
 #ifndef __GLUT
 #define __GLUT
 
+int subWindow;
+int mainWindow;
+
 void initGlut ();
 void initGlutWindow (int iWidth, int iHeight);
 
