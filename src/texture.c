9eab2f6ed5755d3d617fbbcb6373951046a4f44e
andrew.botros <andrew.botros@1f48e373-bc42-0410-b37a-178097bd8f8e>
2008-01-23 06:13:34 +0000

    texture

--- src/texture.c
+++ src/texture.c
@@ -5,6 +5,7 @@
 #include <GL/freeglut.h>
 #include "global.h"
 
+
 unsigned int textureFile (const char *pcFile, int iWidth, int iHeight)
 {
 	unsigned int uiTexture;
@@ -34,3 +35,58 @@ unsigned int textureFile (const char *pcFile, int iWidth, int iHeight)
 
 	return uiTexture;
 }
+
+
+GLuint LoadTextureRAW( const char * filename, int wrap )
+{
+  GLuint texture;
+  int width, height;
+  unsigned char * data;
+  FILE * file;
+
+  // open texture data
+  file = fopen( filename, "rb" );
+  if ( file == NULL ) return 0;
+
+  // allocate buffer
+  width = 256;
+  height = 256;
+  data = malloc( width * height * 3 );
+
+  // read texture data
+  fread( data, width * height * 3, 1, file );
+  fclose( file );
+
+  // allocate a texture name
+  glGenTextures( 1, &texture );
+
+  // select our current texture
+  glBindTexture( GL_TEXTURE_2D, texture );
+
+  // select modulate to mix texture with color for shading
+  glTexEnvf( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE );
+
+  // when texture area is small, bilinear filter the closest MIP map
+  glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                   GL_LINEAR_MIPMAP_NEAREST );
+  // when texture area is large, bilinear filter the first MIP map
+  glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+
+  // if wrap is true, the texture wraps over at the edges (repeat)
+  //       ... false, the texture ends at the edges (clamp)
+  glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,
+                   wrap ? GL_REPEAT : GL_CLAMP );
+  glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T,
+                   wrap ? GL_REPEAT : GL_CLAMP );
+
+  // build our texture MIP maps
+  gluBuild2DMipmaps( GL_TEXTURE_2D, 3, width,
+    height, GL_RGB, GL_UNSIGNED_BYTE, data );
+
+  // free buffer
+  free( data );
+
+  return texture;
+
+}
+
